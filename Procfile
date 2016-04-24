5a8d1161166bb7e5f657452fdde8db3f73cb9819
Namita Mahure <namita@Namitas-MacBook-Pro.local>
2016-04-24 11:38:23 -0500

    first commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: node appExp0.js
---
+++ appExp0.js
@@ -0,0 +1,28 @@
+//require('express')().listen(3000);
+var exp = require('express');
+var app = exp();
+
+//Middleware to make a folder public to serve static content
+app.use(exp.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+    res.send("Hello Express");    
+});
+
+//REST URL
+app.get('/players/:name/:lang', function(req, res){
+    res.write("name="+ req.params.name);
+    res.end(" lang="+ req.params.lang); 
+});
+
+app.get('/players', function(req, res){
+    var query = req.query;
+    console.log("query value :" + JSON.stringify(query));
+    res.write("name="+ query.name);
+    res.end(" lang="+ query.lang);
+    //res.send(JSON.stringify(query));
+});
+var port = process.env.npm_package_config_port || 3000; 
+app.listen(port, function(){
+    console.log("This Server is running on port :" + port);
+});
